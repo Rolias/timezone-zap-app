605ed6534c2ab4ae0c2ac37b1331877ee7cf747b
Tod Gentille <tod.gentille@gmail.com>
2017-06-18 23:16:33 -0700

    beautified

--- creates/timezone.js
+++ creates/timezone.js
@@ -1,5 +1,6 @@
 'use strict';
 const AUTH = require('../_local/google-api-credentials.js');
+var Google = require('../lib/google.js');
 // We recommend writing your creates separate like this and rolling them
 // into the App definition at the end.
 module.exports = {
@@ -15,54 +16,64 @@ module.exports = {
 
   // `operation` is where the business logic goes.
   operation: {
-    inputFields: [
-      {key: 'latitude', required: true, type: 'string', helpText:'Latitude of location for the timezone.'},
-      {key: 'longitude', required: true, type: 'string', helpText: 'Longitude of the location for the timezone.'},
+    inputFields: [{
+        key: 'latitude',
+        required: true,
+        type: 'string',
+        helpText: 'Latitude of location for the timezone.'
+      },
+      {
+        key: 'longitude',
+        required: true,
+        type: 'string',
+        helpText: 'Longitude of the location for the timezone.'
+      },
     ],
 
     perform: (z, bundle) => {
-      const googleUrl = "https://maps.googleapis.com/maps/api/timezone/json?", 
-            location = "location="+bundle.inputData.latitude+","+bundle.inputData.longitude,
-            currentDate = new Date(),
-            MSECS_PER_SEC = 1000,
-            currentTime = currentDate.getTime(),
-            timeAsSecs = parseInt(currentTime/MSECS_PER_SEC),
-            timestamp= "&timestamp="+timeAsSecs,
-            googleKey = "&key="+AUTH.googleApiKey,  
-            googleCmd = googleUrl+location+timestamp+googleKey,
-            promise = z.request(googleCmd, {
-            });
-            //console.log ("GoogleCmd is:"+googleCmd);
-           //this.util();
+      return Google.getTimeZoneByLatLon(z, bundle.inputData.latitude, bundle.inputData.longitude);
+      // const googleUrl = "https://maps.googleapis.com/maps/api/timezone/json?", 
+      //       location = "location="+bundle.inputData.latitude+","+bundle.inputData.longitude,
+      //       currentDate = new Date(),
+      //       MSECS_PER_SEC = 1000,
+      //       currentTime = currentDate.getTime(),
+      //       timeAsSecs = parseInt(currentTime/MSECS_PER_SEC),
+      //       timestamp= "&timestamp="+timeAsSecs,
+      //       googleKey = "&key="+AUTH.googleApiKey,  
+      //       googleCmd = googleUrl+location+timestamp+googleKey,
+      //       promise = z.request(googleCmd, {
+      //       });
+      //       //console.log ("GoogleCmd is:"+googleCmd);
+      //      //this.util();
 
-      return promise.then((response) => {
-        const SECS_PER_HOUR = 3600;
-        var currentHour = currentDate.getHours(),
-            timezoneObj =  JSON.parse(response.content);
-        timezoneObj.totalOffset = timezoneObj.rawOffset + timezoneObj.dstOffset;
-        var offsetHours = timezoneObj.totalOffset/SECS_PER_HOUR;
-        console.log(currentHour);
-        console.log(offsetHours);
-        var adjHour = currentHour + offsetHours;
-        
-        if (adjHour < 0){
-          adjHour= 24+adjHour;
-        }
-        timezoneObj.adjHoursOffset = adjHour;
-        var localTimestamp = currentTime +(timezoneObj.totalOffset * MSECS_PER_SEC);
-        timezoneObj.localTimestampMs = localTimestamp;
-        console.log ("type is "+ typeof timezoneObj.localTimestampMs);
+      // return promise.then((response) => {
+      //   const SECS_PER_HOUR = 3600;
+      //   var currentHour = currentDate.getHours(),
+      //       timezoneObj =  JSON.parse(response.content);
+      //   timezoneObj.totalOffset = timezoneObj.rawOffset + timezoneObj.dstOffset;
+      //   var offsetHours = timezoneObj.totalOffset/SECS_PER_HOUR;
+      //   console.log(currentHour);
+      //   console.log(offsetHours);
+      //   var adjHour = currentHour + offsetHours;
 
-        console.log(timezoneObj);
+      //   if (adjHour < 0){
+      //     adjHour= 24+adjHour;
+      //   }
+      //   timezoneObj.adjHoursOffset = adjHour;
+      //   var localTimestamp = currentTime +(timezoneObj.totalOffset * MSECS_PER_SEC);
+      //   timezoneObj.localTimestampMs = localTimestamp;
+      //   console.log ("type is "+ typeof timezoneObj.localTimestampMs);
 
-        // Special tests
-        var testDate = new Date(localTimestamp);
-        console.log("Hours: "+ testDate.getHours()+ " Minutes:"+ testDate.getMinutes());
+      //   console.log(timezoneObj);
 
-        return timezoneObj;
-      });  
-  },
-    
+      //   // Special tests
+      //   var testDate = new Date(localTimestamp);
+      //   console.log("Hours: "+ testDate.getHours()+ " Minutes:"+ testDate.getMinutes());
+
+      //   return timezoneObj;
+      // });  
+    },
+
     // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
     // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
     // returned records, and have obviously dummy values that we can show to any user.
@@ -76,20 +87,43 @@ module.exports = {
       adjHoursOffset: 12,
       localTimestampMs: 1497695991304
     },
-    
+
     // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
     // field definitions. The result will be used to augment the sample.
     // outputFields: () => { return []; }
     // Alternatively, a static field definition should be provided, to specify labels for the fields
-    outputFields: [
-      {key: 'dstOffset', label: 'Daylight Savings Time Offset In Seconds'},
-      {key: 'rawOffset', label: 'Timezone offset in Seconds (does not include DST'},
-      {key: 'status', label: 'Status'},
-      {key: 'timeZoneId', label: 'Location name'},
-      {key: 'timeZoneName', label: 'Timezone name (e.g. Pacific Daylight Time'},
-      {key: 'totalOffset', label: 'Timezone raw and dst combined'},
-      {key: 'adjHoursOffset', label:'Hours Value in local timezone'},
-      {key: 'localTimestampMs', label:'Local Time in MS'}
+    outputFields: [{
+        key: 'dstOffset',
+        label: 'Daylight Savings Time Offset In Seconds'
+      },
+      {
+        key: 'rawOffset',
+        label: 'Timezone offset in Seconds (does not include DST'
+      },
+      {
+        key: 'status',
+        label: 'Status'
+      },
+      {
+        key: 'timeZoneId',
+        label: 'Location name'
+      },
+      {
+        key: 'timeZoneName',
+        label: 'Timezone name (e.g. Pacific Daylight Time'
+      },
+      {
+        key: 'totalOffset',
+        label: 'Timezone raw and dst combined'
+      },
+      {
+        key: 'adjHoursOffset',
+        label: 'Hours Value in local timezone'
+      },
+      {
+        key: 'localTimestampMs',
+        label: 'Local Time in MS'
+      }
     ]
   }
 };
